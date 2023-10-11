e181fc8e9094ffc88a76d6ffd7b619039fcecb12
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-10-11 11:27:18 -0700

    added initial html layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./header.css";
+export default function Header() {
+  return (
+    <div className="header">
+      <p>about</p>
+      <p>projects</p>
+      <p>resume</p>
+      <p>connect</p>
+    </div>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css";
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Hi I&apos;m Sebastian</h2>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias rem,
+        aliquam laborum mollitia excepturi quis sapiente asperiores perspiciatis
+        natus alias maiores pariatur vel id. Temporibus consectetur facere
+        doloremque nemo saepe?
+      </div>
+    </div>
+  );
+}
